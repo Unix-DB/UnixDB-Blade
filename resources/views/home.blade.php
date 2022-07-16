08a271d6716906ad933af7818077fc7076aeaac6
WillTheDeveloper <willthedeveloper13@gmail.com>
2022-07-16 11:35:33 +0100

    Updated nav for guest and auth

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -349,10 +349,18 @@
                                     </div>
                                 </div>
                             </nav>
+                            @guest()
                             <div class="flex items-center md:ml-12">
                                 <a href="{{route('login')}}" class="text-base font-medium text-gray-500 hover:text-gray-900"> Sign in </a>
                                 <a href="{{route('register')}}" class="ml-8 inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700"> Sign up </a>
                             </div>
+                            @endguest()
+                            @auth()
+                                <div class="flex items-center md:ml-12">
+                                    <a href="{{route('login')}}" class="text-base font-medium text-gray-500 hover:text-gray-900"> Dashboard </a>
+                                    <a href="{{route('register')}}" class="ml-8 inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700"> New post </a>
+                                </div>
+                            @endauth
                         </div>
                     </div>
                 </div>
