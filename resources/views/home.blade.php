61c074046a02ec22e6a0074af2f22e245325bf80
WillTheDeveloper <willthedeveloper13@gmail.com>
2022-07-16 11:42:20 +0100

    Updated nav button to latest posts

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -76,10 +76,10 @@
                                                     </div>
                                                     <div class="ml-4 md:flex-1 md:flex md:flex-col md:justify-between lg:ml-0 lg:mt-4">
                                                         <div>
-                                                            <p class="text-base font-medium text-gray-900">Analytics</p>
-                                                            <p class="mt-1 text-sm text-gray-500">Get a better understanding of where your traffic is coming from.</p>
+                                                            <p class="text-base font-medium text-gray-900">Latest</p>
+                                                            <p class="mt-1 text-sm text-gray-500">View all the lates posts on Unix DB.</p>
                                                         </div>
-                                                        <p class="mt-2 text-sm font-medium text-indigo-600 lg:mt-4">Learn more <span aria-hidden="true">&rarr;</span></p>
+                                                        <p class="mt-2 text-sm font-medium text-indigo-600 lg:mt-4">Latest Posts <span aria-hidden="true">&rarr;</span></p>
                                                     </div>
                                                 </div>
                                             </a>
@@ -143,40 +143,6 @@
                                                     </div>
                                                 </div>
                                             </a>
-                                        </div>
-                                        <div class="bg-gray-50">
-                                            <div class="max-w-7xl mx-auto space-y-6 px-4 py-5 sm:flex sm:space-y-0 sm:space-x-10 sm:px-6 lg:px-8">
-                                                <div class="flow-root">
-                                                    <a href="#" class="-m-3 p-3 flex items-center rounded-md text-base font-medium text-gray-900 hover:bg-gray-100">
-                                                        <!-- Heroicon name: outline/play -->
-                                                        <svg class="flex-shrink-0 h-6 w-6 text-gray-400" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" aria-hidden="true">
-                                                            <path stroke-linecap="round" stroke-linejoin="round" d="M14.752 11.168l-3.197-2.132A1 1 0 0010 9.87v4.263a1 1 0 001.555.832l3.197-2.132a1 1 0 000-1.664z" />
-                                                            <path stroke-linecap="round" stroke-linejoin="round" d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
-                                                        </svg>
-                                                        <span class="ml-3">Watch Demo</span>
-                                                    </a>
-                                                </div>
-
-                                                <div class="flow-root">
-                                                    <a href="#" class="-m-3 p-3 flex items-center rounded-md text-base font-medium text-gray-900 hover:bg-gray-100">
-                                                        <!-- Heroicon name: outline/check-circle -->
-                                                        <svg class="flex-shrink-0 h-6 w-6 text-gray-400" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" aria-hidden="true">
-                                                            <path stroke-linecap="round" stroke-linejoin="round" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
-                                                        </svg>
-                                                        <span class="ml-3">View All Products</span>
-                                                    </a>
-                                                </div>
-
-                                                <div class="flow-root">
-                                                    <a href="#" class="-m-3 p-3 flex items-center rounded-md text-base font-medium text-gray-900 hover:bg-gray-100">
-                                                        <!-- Heroicon name: outline/phone -->
-                                                        <svg class="flex-shrink-0 h-6 w-6 text-gray-400" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" aria-hidden="true">
-                                                            <path stroke-linecap="round" stroke-linejoin="round" d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z" />
-                                                        </svg>
-                                                        <span class="ml-3">Contact Sales</span>
-                                                    </a>
-                                                </div>
-                                            </div>
                                         </div>
                                     </div>
                                 </div>
