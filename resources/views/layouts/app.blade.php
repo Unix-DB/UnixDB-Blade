1fb4628fdd44ae83b2ebbdf46e022f5ea19235cc
WillTheDeveloper <willthedeveloper13@gmail.com>
2022-07-16 12:41:28 +0100

    Redesigned the main pages for authenticated users

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+{{--<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">--}}
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -21,7 +21,7 @@
         <x-jet-banner />
 
         <div class="min-h-screen bg-gray-100">
-            @livewire('navigation-menu')
+            {{--@livewire('navigation-menu')
 
             <!-- Page Heading -->
             @if (isset($header))
@@ -30,7 +30,7 @@
                         {{ $header }}
                     </div>
                 </header>
-            @endif
+            @endif--}}
 
             <!-- Page Content -->
             <main>
