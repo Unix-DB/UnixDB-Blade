cb1abfd47d03d2f9bc02efe895096bb230eebbe0
WillTheDeveloper <willthedeveloper13@gmail.com>
2022-07-16 20:15:54 +0100

    Updated terminal migration; removed creator field

--- database/migrations/2022_07_06_200006_create_terminals_table.php
+++ database/migrations/2022_07_06_200006_create_terminals_table.php
@@ -16,7 +16,6 @@ return new class extends Migration
         Schema::create('terminals', function (Blueprint $table) {
             $table->id();
             $table->text('name');
-            $table->text('creator');
             $table->timestamps();
         });
     }
